0d85da635d9269abe9f11a8fcf343b8fc9bb4158
rickc <rickcrist99@gmail.com>
2025-05-20 11:37:52 -0600

    Draw Rectangle

---
+++ Shapes/Rectangle.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+import sys
+
+class Rectangle(QGraphicsRectItem):
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
+
+        self.setBrush(QBrush(Qt.GlobalColor.blue))
+        self.setPen(QPen(Qt.GlobalColor.white, 3))
+        self.setFlags(
+            QGraphicsItem.GraphicsItemFlag.ItemIsMovable |
+            QGraphicsItem.GraphicsItemFlag.ItemIsSelectable
+        )
--- main.py
+++ main.py
@@ -3,11 +3,26 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 import sys
 
+from Shapes.Rectangle import Rectangle
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Digram Editor Tool")
         self.setGeometry(200, 100, 1200, 600)
+
+        # Create a QGraphicsScene
+        self.scene = QGraphicsScene(self)
+        self.scene.setSceneRect(0, 0, 1000, 500)
+
+        # Add a rectangle item to the scene
+        self.shape_item = Rectangle(50, 50, 200, 100)
+        self.scene.addItem(self.shape_item)
+
+        # Create a QGraphicsView, set the scene, and set as central widget
+        self.view = QGraphicsView(self.scene, self)
+        self.setCentralWidget(self.view)
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
